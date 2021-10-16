57e8b310c9355181fdf261c4d06cdc46af0ae061
Shubham Mahajan <mahajanshubham9987@gmail.com>
2021-10-16 18:24:48 +0530

    first test case completed which include adding ,1,1+2

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,6 +3,18 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	if(input.equals(""))
+    		return 0;
+    	else if(!input.contains(","))
+    		return Integer.parseInt(input);
+    	else if(input.contains(","))
+    	{
+    		String temp[]=input.split(",");
+    		
+    		int a=Integer.parseInt(temp[0]);
+    		int b=Integer.parseInt(temp[1]);
+    		return a+b;
+    	}
         return 0;
     }
 
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
-
+//Question
     @Test
     void empty_string_should_return_0() {
         StringCalculator stringCalculator = new StringCalculator();
@@ -16,5 +16,11 @@ class StringCalculatorShould {
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+    
+    @Test
+    void string_with_multiple_number_should_return_number_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
     }
 }
